6bfb6b3398d11d6352038765bf6716be0487a2dd
Arif <iariful950@gmail.com>
2022-02-17 17:39:45 +0600

    everything done but i might change something!

--- js/script.js
+++ js/script.js
@@ -1,47 +1,57 @@
+// checking type error and negative number error
 function checkError(inputField) {
     const input = document.getElementById(inputField).value;
     if (parseFloat(input) < 0) {
         document.getElementById('negative-val-error').style.display = 'block';
+        document.getElementById('number-error-text').style.display = 'none';
+        document.getElementById('empty-value-error').style.display = 'none';
+        document.getElementById('expense-income').style.display = 'none';
+        document.getElementById('save-less').style.display = 'none';
     }
     else if (isNaN(input) == true) {
         document.getElementById('number-error-text').style.display = 'block';
+        document.getElementById('negative-val-error').style.display = 'none';
+        document.getElementById('empty-value-error').style.display = 'none';
+        document.getElementById('expense-income').style.display = 'none';
+        document.getElementById('save-less').style.display = 'none';
+    }
+    else if (input == '') {
+        document.getElementById('empty-value-error').style.display = 'block';
+        document.getElementById('negative-val-error').style.display = 'none';
+        document.getElementById('number-error-text').style.display = 'none';
+        document.getElementById('expense-income').style.display = 'none';
+        document.getElementById('save-less').style.display = 'none';
     }
 }
 
-// function numberTypeCheck(inputField) {
-//     const input = document.getElementById(inputField).value;
-//     if (isNaN(input) == true) {
-//         document.getElementById('number-error-text').style.display = 'block';
-//     }
-// }
-
+// get income 
 function extractIncome() {
     const incomeInput = document.getElementById('income-input');
-    // numberTypeCheck('income-input');
     checkError('income-input');
     const income = parseFloat(incomeInput.value);
     return income;
 }
 
-
+// calculate total expenses and balance
 function calculateBalance() {
     const income = extractIncome();
     const foodInput = document.getElementById('food-input');
-    // numberTypeCheck('food-input');
     checkError('food-input');
     const foodCost = parseFloat(foodInput.value);
     const rentInput = document.getElementById('rent-input');
-    // numberTypeCheck('rent-input');
     checkError('rent-input');
     const rentCost = parseFloat(rentInput.value);
     const clothesInput = document.getElementById('clothes-input');
-    // numberTypeCheck('clothes-input');
     checkError('clothes-input');
     const clothesCost = parseFloat(clothesInput.value);
-
     const totalExpenses = foodCost + rentCost + clothesCost;
+    // showing error if total expenses are higher than income
     if (totalExpenses > income) {
         document.getElementById('expense-income').style.display = 'block';
+        document.getElementById('negative-val-error').style.display = 'none';
+        document.getElementById('number-error-text').style.display = 'none';
+        document.getElementById('empty-value-error').style.display = 'none';
+        document.getElementById('save-less').style.display = 'none';
     }
     const totalExpensesText = document.getElementById('total-expenses');
     totalExpensesText.innerText = totalExpenses;
@@ -50,17 +60,22 @@ function calculateBalance() {
     balanceText.innerText = balance;
 }
 
+// calculate saving amount and remaining balance
 function calculateRemainingBalance() {
     const income = extractIncome();
     const saveInput = document.getElementById('save-input');
-    // numberTypeCheck('save-input');
     checkError('save-input');
     const save = parseFloat(saveInput.value);
     const savingAmount = income * (save / 100);
     const savingAmountText = document.getElementById('saving-amount');
     const balance = parseFloat(document.getElementById('balance').innerText);
+    // showing error if saving amount is higher than current balance
     if (savingAmount > balance) {
         document.getElementById('save-less').style.display = 'block';
+        document.getElementById('negative-val-error').style.display = 'none';
+        document.getElementById('number-error-text').style.display = 'none';
+        document.getElementById('empty-value-error').style.display = 'none';
+        document.getElementById('expense-income').style.display = 'none';
     }
     savingAmountText.innerText = savingAmount;
     const remainingBalance = balance - savingAmount;
@@ -68,10 +83,12 @@ function calculateRemainingBalance() {
     remainingBalanceText.innerText = remainingBalance;
 }
 
+// calculate button event handler
 document.getElementById('calc-btn').addEventListener('click', function () {
     calculateBalance();
 });
 
+// save button event handler
 document.getElementById('save-btn').addEventListener('click', function () {
     calculateRemainingBalance();
 });
